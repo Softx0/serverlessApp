a963c650c0d3895cc9b0b074326f547674c9cce5
Eduardo <ing.valen0421@hotmail.com>
2021-01-24 18:25:59 -0400

    Refactorizando, utilizando template strings

--- almuerzi-web/js/main.js
+++ almuerzi-web/js/main.js
@@ -14,11 +14,16 @@ window.onload = () => {
     //     .then(response => response.json()) //.json, .text, .xml
     //     .then(data => console.log(data));
 
+    //templates strings
+    const renderItem = (item) => {
+        return `<li data-id="${item._id}" >${item.name}</li>`;
+    }
+
     fetch('https://serverless-app.softx0.vercel.app/api/meals')
         .then(response => response.json()) //.json, .text, .xml
         .then(data => {
             const mealsList = document.getElementById('meals-list');
-            const template = data.map(x => '<li>' + x.name + '</li>').join('');
+            const template = data.map(renderItem).join('');
             const submit = document.getElementById('submit');
 
             mealsList.innerHTML = template;

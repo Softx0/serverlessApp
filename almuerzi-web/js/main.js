a47c52271de0dc251442138cf9c58a26252ff856
Eduardo <ing.valen0421@hotmail.com>
2021-01-23 23:57:20 -0400

    Consumiendo api get de meals listo

---
+++ almuerzi-web/js/main.js
@@ -0,0 +1,26 @@
+
+window.onload = () => {
+    // fetch('https://serverless-app.softx0.vercel.app/api/meals', {
+    //     method: 'GET', //post, put, delete
+    //     mode: 'cors',
+    //     cache: 'no-cache',
+    //     credentials: 'same-origin',
+    //     headers: {
+    //         'Conten-Type': 'application/json'
+    //     },
+    //     redirect: 'follow',
+    //     body: JSON.stringify({user: 'lala', pass: 'hola'})
+    // })
+    //     .then(response => response.json()) //.json, .text, .xml
+    //     .then(data => console.log(data));
+
+    fetch('https://serverless-app.softx0.vercel.app/api/meals')
+        .then(response => response.json()) //.json, .text, .xml
+        .then(data => {
+            const mealsList = document.getElementById('meals-list');
+            const template = data.map(x => '<li>' + x.name + '</li>').join('');
+            
+            mealsList.innerHTML = template
+        } );
+}
+

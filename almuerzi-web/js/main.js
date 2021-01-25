92dcf93208cbd62239ead1175c60505199825dd8
Eduardo <ing.valen0421@hotmail.com>
2021-01-24 20:06:03 -0400

    Creando la orden listo

--- almuerzi-web/js/main.js
+++ almuerzi-web/js/main.js
@@ -1,20 +1,22 @@
+let mealsState = [];
+
 const stringToHTML = (cadena) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(cadena, 'text/html');
-    
+
     return doc.body.firstChild;
 }
 
 //templates strings
 const renderItem = (item) => {
     const element = stringToHTML(`<li data-id="${item._id}" >${item.name}</li>`);
-    
+
     //añadiendo la principal funcionalidad de seleccion de los platos...
     element.addEventListener('click', () => {
         const mealsList = document.getElementById('meals-list');
         const mealsIdInput = document.getElementById('mealsId');
 
-        Array.from(mealsList.children).forEach( x => x.classList.remove('selected'));
+        Array.from(mealsList.children).forEach(x => x.classList.remove('selected'));
         element.classList.add('selected');
 
         mealsIdInput.value = item._id;
@@ -23,65 +25,83 @@ const renderItem = (item) => {
     return element;
 }
 
+const renderOrder = (order, meals) => {
+    const meal = meals.find(meal => meal._id === order.meal_id);
+    const element = stringToHTML(`<li data-id="${order._id}" >${meal.name} - ${order.user_id}</li>`);
+
+    return element;
+}
+
 window.onload = () => {
 
     const orderForm = document.getElementById('order');
+
     orderForm.onsubmit = (event) => {
         //para evitar el refresh
         event.preventDefault();
 
+        const submit = document.getElementById('submit');
         const mealId = document.getElementById('mealsId');
         const mealIdValue = mealId.value;
-        
-        if(!mealIdValue){
+
+        submit.setAttribute('disabled', true);
+
+        if (!mealIdValue) {
             alert('Debe seleccionar un plato');
             return
         }
 
         const order = {
             meal_id: mealIdValue,
-            user_id: 'Emmanuel',
+            user_id: 'Shirley',
         }
 
-        fetch('https://serverless-app.softx0.vercel.app/api/orders', {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(order),
+        fetch(`https://serverless-app.softx0.vercel.app/api/orders`, {
             method: 'POST',
-        }).then(x => console.log(x))
-    }
+            headers: {
+                'Content-Type': 'application/json'
+                // Authorization: token,
+            },
+            body: JSON.stringify(order)
+        }).then(x => x.json())
+            .then(respuesta => {
+                const renderedOrder = renderOrder(respuesta, mealsState);
+                const ordersList = document.getElementById('orders-list');
 
-    // fetch('https://serverless-app.softx0.vercel.app/api/meals', {
-    //     method: 'GET', //post, put, delete
-    //     mode: 'cors',
-    //     cache: 'no-cache',
-    //     credentials: 'same-origin',
-    //     headers: {
-    //         'Conten-Type': 'application/json'
-    //     },
-    //     redirect: 'follow',
-    //     body: JSON.stringify({user: 'lala', pass: 'hola'})
-    // })
-    //     .then(response => response.json()) //.json, .text, .xml
-    //     .then(data => console.log(data));
+                ordersList.appendChild(renderedOrder);
+                submit.removeAttribute('disabled', false);
+            });
+    }
 
     fetch('https://serverless-app.softx0.vercel.app/api/meals')
         .then(response => response.json()) //.json, .text, .xml
         .then(data => {
-
+            mealsState = data;
             const mealsList = document.getElementById('meals-list');
             const submit = document.getElementById('submit');
             const listItems = data.map(renderItem);
-            
+
             mealsList.removeChild(mealsList.firstElementChild);
             listItems.forEach(element => {
                 mealsList.appendChild(element);
             });
 
             submit.removeAttribute('disabled');
+
+            fetch('https://serverless-app.softx0.vercel.app/api/orders')
+                .then(response => response.json())
+                .then(ordersData => {
+                    const ordersList = document.getElementById('orders-list');
+                    const listOrders = ordersData
+                        .map(orderData => renderOrder(orderData, mealsState));
+
+                    ordersList.removeChild(ordersList.firstElementChild);
+                    listOrders.forEach(order => {
+                        ordersList.appendChild(order);
+                    });
+                });
         });
 
-    
+
 }
 
